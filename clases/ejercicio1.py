5eba932c917acbebb76c670362a2e8e8d27413e0
jzazooro <91785177+jzazooro@users.noreply.github.com>
2022-04-17 15:23:01 +0200

    clases MientrasQue y Mostrar

--- clases/ejercicio1.py
+++ clases/ejercicio1.py
@@ -1,10 +1,20 @@
-class bloque:
+class Bloque:
     def __init__(self):
         self.instrucciones=[]
     def agregarinstrucciones(self, instruccion):
         self.instrucciones.append(instruccion)
+
 class Si:
     def __init__(self, condicion, entonces, si_no):
         self.condicion=condicion
         self.entonces=entonces
         self.si_no=si_no
+
+class MientrasQue:
+    def __init__(self, condicion, bloque):
+        self.condicion=condicion
+        self.bloque=bloque
+
+class Mostrar:
+    def __init__(self, mensaje):
+        self.mensaje=mensaje
