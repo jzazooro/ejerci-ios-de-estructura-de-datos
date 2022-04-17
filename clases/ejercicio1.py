0abdd1d78e58264e328b9e4bd56913440af5b6e2
jzazooro <91785177+jzazooro@users.noreply.github.com>
2022-04-17 15:20:11 +0200

    clase Si

--- clases/ejercicio1.py
+++ clases/ejercicio1.py
@@ -1,5 +1,10 @@
 class bloque:
     def __init__(self):
         self.instrucciones=[]
-    def agregarinstrucciones(self, instrucciones):
+    def agregarinstrucciones(self, instruccion):
         self.instrucciones.append(instruccion)
+class Si:
+    def __init__(self, condicion, entonces, si_no):
+        self.condicion=condicion
+        self.entonces=entonces
+        self.si_no=si_no
