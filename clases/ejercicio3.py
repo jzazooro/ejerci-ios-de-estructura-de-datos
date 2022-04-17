2d7d28d6287ddf4c082c323a1cc3f28a433eaa71
jzazooro <91785177+jzazooro@users.noreply.github.com>
2022-04-17 15:41:01 +0200

    ejercicio 2 hemos añadido las clases factoryfactura y crear

--- clases/ejercicio3.py
+++ clases/ejercicio3.py
@@ -6,7 +6,14 @@ class naturaleza:
 class producto(naturaleza):
     def __init__(self, tasaiva):
         self.tasaiva=tasaiva
+
     def facturar(self):
         return 100 + 100*self.tasaiva
 
+class factoryfactura(producto):
+    def __init__(self):
+        pass
 
+    def crear(self):
+        clase=producto(self.tasaiva)
+        return clase
